bed472d22664730d18efa69202802c24d89174cf
URIDIOS <xfalokkopax@gmail.com>
2025-01-22 18:45:41 -0700

    imc

---
+++ MauiApp1_JUOO1/MauiApp1_JUOO1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp1_JUOO1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiApp1_JUOO1/MauiApp1_JUOO1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace MauiApp1_JUOO1
+{
+    public partial class MainPage : ContentPage
+    {
+        const double UnderweightThreshold = 18.5;
+        const double NormalWeightThreshold = 24.9;
+        const double OverweightThreshold = 29.9;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+            var weight = double.Parse(Kilos.Text);
+            var height = double.Parse(Altura.Text) / 100;
+
+            var imc = weight / (height * height);
+
+            IMC.Text = imc.ToString("F2");
+
+            string result = GetImc(imc);
+            DisplayAlert("Resultado",result,"OK");
+
+		}
+        private string GetImc(double imc)
+        {
+            if(imc < UnderweightThreshold)
+            {
+                return "Bajo peso";
+            }
+            else if (imc <= NormalWeightThreshold)
+            {
+                return "Peso normal";
+            }
+            else if (imc <= OverweightThreshold)
+            {
+                return "Sobre peso";
+            }
+            else
+            {
+                return "Demaciado peso";
+            }
+        }
+	}
+
+}
